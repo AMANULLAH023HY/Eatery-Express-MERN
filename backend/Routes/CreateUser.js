e258c5ae177744ec3dd576c5b7e7ebf72021cf9a
AMANULLAH023HY <amana170829@gmail.com>
2024-02-01 23:51:50 +0530

    complete bcrypt and jwtToken

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const User = require("../Models/User");
 
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = "gjkdasjajaaqertygvcdj";
 
 router.post(
   "/createuser",
@@ -18,13 +22,20 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
+const salt = await bcrypt.genSalt(10);
+const secPassword = await bcrypt.hash(req.body.password,salt)
     try {
-      User.create({
+       await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPassword,
         location: req.body.location,
+
+
+        // name:"aman",
+        // email:"aman@gmail.com",
+        // password:"654321",
+        // location:"Delhi"
       });
 
       res.status(200).json({
@@ -68,15 +79,25 @@ router.post(
 
 
        }
-       if(req.body.password !== userData.password){
+       const pwsCompare = await bcrypt.compare(req.body.password,userData.password)
+             if(!pwsCompare){
         res.status(400).json({
             success: flase,
             message: "Try login with correct credentials",
           });
        }
 
+       const data={
+        user:{
+          id:userData.id
+        }
+       }
+
+       const authToken = jwt.sign(data,jwtSecret)
+
        res.status(200).json({
         success: true,
+        authToken:authToken,
         message: "User loggedin successfully!",
       });
 
--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,74 +1,84 @@
-import React, { useState } from 'react'
-import { Link,useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [credential, setCredential] = useState({email:"",password:""});
+  const [credential, setCredential] = useState({ email: "", password: "" });
 
-  const handleSubmit =async(e)=>{
-      e.preventDefault();
-      const response =await fetch("http://localhost:5000/api/loginuser",{
-          method:"POST",
-          hesders:{
-              'Content-Type':'application/json'
-          },
-          body:JSON.stringify({
-            email:credential.email,
-            password:credential.password,
- })
-      });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/loginuser", {
+      method: "POST",
+      hesders: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credential.email,
+        password: credential.password,
+      }),
+    });
 
-      const json = await response.json();
-      console.log(json);
+    const json = await response.json();
+    console.log(json);
 
-
-      if(!json.success){
-          alert("Enter valid credentials")
-      }
-
-      if(json.success){
-        
-        navigate('/')
+    if (!json.success) {
+      alert("Enter valid credentials");
     }
-      
-      
 
-  }
+    if (json.success) {
+      localStorage.setItem("authToken",json.authToken)
+      navigate("/");
+    }
+  };
 
-
-  const onChange = (event)=>{
-setCredential({...credential,[event.target.name]:event.target.value})
- 
-  }
+  const onChange = (event) => {
+    setCredential({ ...credential, [event.target.name]: event.target.value });
+  };
 
   return (
     <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              name="email"
+              value={credential.email}
+              onChange={onChange}
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              value={credential.password}
+              onChange={onChange}
+            />
+          </div>
 
-<div className="container">
-
-<form onSubmit={handleSubmit}>
-
-
-
-<div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={credential.email} onChange={onChange}/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" name="password" value = {credential.password} onChange={onChange}/>
-  </div>
-
-
-  
-  
-  <button type="submit" className="btn btn-success">Submit</button>
-  <Link to='/createuser' className="m-3 btn btn-danger">I'm a new user</Link>
-</form>
-</div>
-
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
+          <Link to="/createuser" className="m-3 btn btn-danger">
+            I'm a new user
+          </Link>
+        </form>
+      </div>
     </>
-  )
+  );
 }
