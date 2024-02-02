9309ed39211d8aafcafc8b5678d628c11d97d6bf
AMANULLAH023HY <amana170829@gmail.com>
2024-02-02 15:39:00 +0530

    fetch data using route

---
+++ backend/Routes/DisplayData.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+router.post('/foodData',(req,res)=>{
+    try {
+console.log(global.food_items)
+        res.send([global.food_items])
+        
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            message:"Data not Fetched"
+        })
+    }
+})
+
+module.exports = router;
