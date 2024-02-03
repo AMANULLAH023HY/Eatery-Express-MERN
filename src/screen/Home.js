745b5c857daa4513ee06f8c08891a874453618e2
AMANULLAH023HY <amana170829@gmail.com>
2024-02-03 11:46:43 +0530

    Search Functionality

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
-import Carousel from "../components/Carousel";
+// import Carousel from "../components/Carousel";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
 
@@ -33,50 +35,132 @@ export default function Home() {
   }, []);
 
   return (
+    <div>
+      <div>
+        <NavBar></NavBar>
+      </div>
+      <div>
+        {/* <Carousel></Carousel> */}
+
         <div>
-            <div>
-                <NavBar></NavBar>
+          <div
+            id="carouselExampleFade"
+            class="carousel slide carousel-fade"
+            data-bs-ride="carousel"
+            style={{ objectFit: "contain !important" }}
+          >
+            <div class="carousel-inner" id="carousel">
+              <div class="carousel-caption " style={{ zIndex: "10" }}>
+                <div class="d-flex justify-content-center">
+                  <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={search}
+                    onChange={(e) => {
+                      setSearch(e.target.value);
+                    }}
+                  />
+                  {/* <button class="btn btn-outline-success text-white" type="submit">Search</button> */}
+                </div>
+              </div>
+
+              <div class="carousel-item active">
+                <img
+                  src="https://source.unsplash.com/random/300×300/?burger"
+                  class="d-block w-100"
+                  alt="..."
+                  style={{ filter: "brightness(30%)" }}
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://source.unsplash.com/random/300×300/?sea"
+                  class="d-block w-100"
+                  alt="..."
+                  style={{ filter: "brightness(30%)" }}
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  src="https://source.unsplash.com/random/960×700/?pastry"
+                  class="d-block w-100"
+                  alt="..."
+                  style={{ filter: "brightness(30%)" }}
+                />
+              </div>
             </div>
-            <div>
-                <Carousel></Carousel>
-            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleFade"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleFade"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+      </div>
 
-            <div className="container">
-            {
-                foodCat !==[]
-                ?foodCat.map((data)=>{ 
-                    return(
-                        <div className="row mb-3">                       
-                         <div className="fs-3 m-3"
-                          key={data._id}> {data.CategoryName}</div>
+      <div className="container">
+        {foodCat !== []
+          ? foodCat.map((data) => {
+              return (
+                <div className="row mb-3">
+                  <div className="fs-3 m-3" key={data._id}>
+                    {" "}
+                    {data.CategoryName}
+                  </div>
 
-                          <hr></hr>
-                          {
-                            foodItem !==[]
-                            ? foodItem.filter((item)=>item.CategoryName === data.CategoryName).map(filterItems=>{
-                                return(
-                                    <div className="col-12 col-md-6 col-lg-3" key={filterItems._id}>
-                                        <Card
-                                            foodName={filterItems.name}
-                                            options={filterItems.options    }
-                                            imgSrc={filterItems.img}
-                                        />
-                                    </div>
-
-                                )
-                            }):<div>No such data found</div>
-                          }
-
-                        </div>
-
-                    )
-            }):" "
-            }
-       
-            </div>
-            <div>
-                <Footer></Footer>
-            </div>
+                  <hr></hr>
+                  {foodItem !== [] ? (
+  foodItem
+    .filter((item) => 
+      (item.CategoryName === data.CategoryName) &&
+      (item.name.toLowerCase().includes(search.toLowerCase()))
+    )
+    .map((filterItems) => {
+      return (
+        <div
+          className="col-12 col-md-6 col-lg-3"
+          key={filterItems._id}
+        >
+          <Card
+            foodName={filterItems.name}
+            options={filterItems.options}
+            imgSrc={filterItems.img}
+          />
+        </div>
+      );
+    })
+) : (
+  <div>No such data found</div>
+)}
+                </div>
+              );
+            })
+          : " "}
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
