09895fa90b1da53d6279cce647cb116be0826086
AMANULLAH023HY <amana170829@gmail.com>
2024-02-06 23:13:13 +0530

    Add Delete and Update to cart functionality

--- src/components/ContextReducer.js
+++ src/components/ContextReducer.js
@@ -1,44 +1,3 @@
-// import {  createContext, useContext, useReducer } from "react"
-
-
-// const CartStateContext = createContext();
-// const CartDispatchContext = createContext();
-
-// const reducer = (state,action) =>{
-//     switch(action.type){
-//         case "ADD":
-//             return [...state, {id:action.id, name:action.name, img:action.img, size:action.size,qty:action.qty, price:action.price}]
-
-//         default:
-//             console.log("Error in reducer")
-//     }
-
-// }
-
-// export default function CartProvider({children}){
-
-//     const [state, dispatch] = useReducer(reducer)
-//     return(
-
-//         <CartDispatchContext.Provider value={dispatch}
-// >
-//     <CartStateContext.Provider value={state}>
-//         {children}
-//     </CartStateContext.Provider>
-// </CartDispatchContext.Provider>
-
-
-//     )
-// }
-
-// export const useCart = () =>useContext(CartStateContext);
-
-// export const useDispatchCart = ()=>useContext(CartDispatchContext);
-
-
-
-
-
 
 
 import { createContext, useContext, useReducer } from "react";
@@ -60,11 +19,30 @@ const reducer = (state, action) => {
           price: action.price,
         },
       ];
-      case "REMOVE":
-        let newArr = [...state]
-        newArr.splice(action.index,1)
+    case "REMOVE":
+      let newArr = [...state];
+      newArr.splice(action.index, 1);
 
-        return newArr;
+      return newArr;
+
+    case "UPDATE":
+      let arr = [...state]
+      arr.find((food, index) => {
+        if (food.id === action.id) {
+          console.log(
+            food.qty,
+            parseInt(action.qty),
+            action.price + food.price
+          );
+          arr[index] = {
+            ...food,
+            qty: parseInt(action.qty) + food.qty,
+            price: action.price + food.price,
+          };
+        }
+        return arr
+      });
+      return arr
 
     default:
       console.log("Error in reducer");
